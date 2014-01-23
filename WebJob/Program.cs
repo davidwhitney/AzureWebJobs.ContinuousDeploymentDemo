24288f7e039abcef78bfa712d25ab9c9ac55e8d3
David Whitney <david@davidwhitney.co.uk>
2014-01-23 20:33:47 +0000

    Simple job.

---
+++ WebJob/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "webjoblog.txt";
+
+            if (!File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            var fs = File.Create(filename);
+            fs.Close();
+
+            
+            var i = 1;
+            while (true)
+            {
+                using (var w = File.AppendText(filename))
+                {
+                    var msg = "Iteration: " + i;
+                    
+                    Console.WriteLine(msg);
+                    w.WriteLine(msg);
+                }
+
+                i++;
+
+                Thread.Sleep(1000);
+
+                if (i >= 100000)
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
